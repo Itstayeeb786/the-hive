df6b9f3964fd523691114862214186cd92a17d91
itstayeeb786 <sonoftayeb@gmail.com>
2024-08-07 12:15:20 +0300

    Set up the Home Page

---
+++ hiveapp/components/wrappers/background.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import bgimage from "@/public/bg.jpeg";
+
+export default function Background() {
+  return (
+    <>
+      <Image
+        src={bgimage}
+        alt="bg"
+        className="inset-0 fixed z-[-999] w-full h-screen object-cover opacity-70 blur-sm"
+      />
+    </>
+  );
+}
