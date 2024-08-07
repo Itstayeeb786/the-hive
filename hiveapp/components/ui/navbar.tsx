d91122c8018ca2ed1d499f5e98d095a51fb8b463
itstayeeb786 <sonoftayeb@gmail.com>
2024-08-07 15:24:45 +0300

    added the navbar

---
+++ hiveapp/components/ui/navbar.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
+import { cn } from "@/lib/utils";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div className={cn("top-10 inset-x-0 w-9/12 mx-auto z-50", className)}>
+      <Menu setActive={setActive}>
+        <Image
+          src="/applogo.png"
+          alt="Hive Logo"
+          title="THE HIVE"
+          width={150}
+          height={20}
+          priority
+          className=" items-center "
+        />
+        <a href="/" className="mt-5 font-bold">
+          Home
+        </a>
+        <MenuItem setActive={setActive} active={active} item="Businesses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/">Web Development</HoveredLink>
+            <HoveredLink href="/">Interface Design</HoveredLink>
+            <HoveredLink href="/">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/">Branding</HoveredLink>
+          </div>
+        </MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="Register">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/">Hobby</HoveredLink>
+            <HoveredLink href="/">Individual</HoveredLink>
+            <HoveredLink href="/">Team</HoveredLink>
+            <HoveredLink href="/">Enterprise</HoveredLink>
+          </div>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
--- hiveapp/pages/index.tsx
+++ hiveapp/pages/index.tsx
@@ -1,44 +1,38 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Background from "@/components/wrappers/background";
+import React, { useState } from "react";
+import Navbar from "@/components/ui/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+export function NavbarDemo() {
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <Navbar className="top-2" />
+      <p className="text-black dark:text-white">
+        The Navbar will show on top of the page
+      </p>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <Navbar />
       <Background />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Image
-          src="/hive.png"
-          alt="Hive Logo"
-          width={100}
-          height={24}
-          priority
-        />
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Home | About | View Businesses | Register | Sign Up
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Login{" "}
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center ">
+      <div className="relative flex place-items-center mt-12">
         <Image
           src="/lmao2.jpeg"
           alt="Next.js Logo"
           width={1080}
           height={37}
           priority
+          className="rounded-2xl"
         />
         <Image
           src="/hive.png"
@@ -48,17 +42,17 @@ export default function Home() {
           priority
           className="top-6 right-0 text-overlay absolute "
         />
-        <p className="text-overlay absolute top-24 right-40 text-7xl font-bold ">
+        <p className="text-overlay absolute top-24 right-40 text-7xl  font-extrabold ">
           THE HIVE
         </p>
         <div className="text-overlay absolute bottom-0 right-0 flex ">
           <a
             href="3"
-            className="pt-5 group rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-10 hover:dark:bg-neutral-800/30"
+            className="pt-5 group rounded-lg border border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-10 hover:dark:bg-neutral-800/30 mb-10 w-56"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
+            <h2 className={` mb-3 text-2xl font-semibold`}>
               Sign Up{" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
@@ -70,7 +64,7 @@ export default function Home() {
           </a>
           <a
             href="3"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-orange-300 hover:bg-gray-100 hover:dark:border-neutral-10 hover:dark:bg-neutral-800/30"
+            className=" pt-5 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-orange-300 hover:bg-gray-100 hover:dark:border-neutral-10 hover:dark:bg-neutral-800/30 mb-10 w-96"
             target="_blank"
             rel="noopener noreferrer"
           >
